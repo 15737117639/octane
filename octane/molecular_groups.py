4e17784bb81e528bfae5a351e3503a12a6608287
Степан Блищак <stepanblischak@gmail.com>
2017-09-20 01:03:06 +0300

    some change in molecular group module, added tests

--- octane/molecular_groups.py
+++ octane/molecular_groups.py
@@ -5,10 +5,10 @@ class MolecularGroup:
     """ a class that represents a single group of atoms
         that can be a part of a molecule"""
 
-    def __init__(self, smiles_structure, **parametrs):
+    def __init__(self, pattern, **parametrs):
 
-        self._smiles_structure = smiles_structure
-        self._smart_object = Smarts(smiles_structure)
+        self._pattern = pattern
+        self._smart_object = Smarts(pattern)
         self._params = parametrs
 
     def __getattr__(self, attr):
@@ -28,20 +28,23 @@ class MolecularGroup:
 
 
 # a dictionary with different groups (parafins and olefins for now)
-molecular_groups = {
-    'CH3': MolecularGroup('[CH3]', ron=-2.315, mon=-0.202),
-    'CH2': MolecularGroup('[CX2H2]', ron=-8.488, mon=-9.082),
-    'CH': MolecularGroup('[CX3H1]', ron=-0.176, mon=-1.821),
-    'C': MolecularGroup('[CX4H0]', ron=11.94, mon=11.90),
-    '=CH': MolecularGroup('C=[CH1]', ron=0.392, mon=-2.293),
-    '=C': MolecularGroup('C=[CH0]', ron=8.697, mon=2.703),
-    '=CH2': MolecularGroup('C=[CH2]', ron=3.623, mon=-0.254),
-    '=C=': MolecularGroup('C=C=C', ron=-37.37, mon=-42.43),
-    '=tCH': MolecularGroup('C=[CH1]', ron=6.449, mon=4.743),
-    '=cCH': MolecularGroup('C=[CH1]', ron=6.269, mon=2.725),
-    '#CH': MolecularGroup('C#[CH1]', ron=18.36, mon=21.36),
-    '#C': MolecularGroup('C#[CH0]', ron=-7.20, mon=-12.96),
+_molecular_groups = {
+    'CH3': {'pattern': '[CH3]', 'ron': -2.315, 'mon': -0.202},
+    'CH2': {'pattern': '[CX4&CH2]', 'ron': -8.488, 'mon': -9.082},
+    'CH': {'pattern': '[CX4&CH1]', 'ron': -0.176, 'mon': -1.821},
+    'C': {'pattern': '[CX4&CH0]', 'ron': 11.94, 'mon': 11.90},
+    '=CH': {'pattern': 'C=[CH1]', 'ron': 0.392, 'mon': -2.293},
+    '=C': {'pattern': 'C=[CH0]', 'ron': 8.697, 'mon': 2.703},
+    '=CH2': {'pattern': 'C=[CH2]', 'ron': 3.623, 'mon': -0.254},
+    '=C=': {'pattern': 'C=C=C', 'ron': -37.37, 'mon': -42.43},
+    '=tCH': {'pattern': 'C=[CH1]', 'ron': 6.449, 'mon': 4.743},
+    '=cCH': {'pattern': 'C=[CH1]', 'ron': 6.269, 'mon': 2.725},
+    '#CH': {'pattern': 'C#[CH1]', 'ron': 18.36, 'mon': 21.36},
+    '#C': {'pattern': 'C#[CH0]', 'ron': -7.20, 'mon': -12.96},
 }
+
+molecular_groups = {key: MolecularGroup(**_molecular_groups[key])
+                    for key in _molecular_groups}
 
 
 def split_molecule_to_substructures(smiles_molecule):
--- tests/test_molecular_groups.py
+++ tests/test_molecular_groups.py
@@ -3,18 +3,25 @@ import unittest
 from octane.molecular_groups import split_molecule_to_substructures
 
 
+# dummy test
 class TestMolecularGroups(unittest.TestCase):
-    tests_cases = {
+
+    test_cases = {
         'CCC': {'CH3': 2, 'CH2': 1},
         'CCCC': {'CH3': 2, 'CH2': 2},
         'CC(C)C': {'CH3': 3, 'CH': 1},
         'CC(C)(C)C': {'CH3': 4, 'C': 1},
         'CC=C': {'CH3': 1, '=CH': 1, '=CH2': 1},
     }
-    def test_cases(self):
-        for test_case in TestMolecularGroups.test_cases:
-            self.assertEqual(split_molecule_to_substructures, False)
 
+    def test(self):
+        for molecule in self.test_cases:
+            supers = split_molecule_to_substructures(molecule)
+            sub = self.test_cases[molecule]
+            # get common elements of dictionaries
+            common = {key: supers[key]
+                      for key in set(supers.keys()).intersection(set(sub.keys()))}
+            self.assertEqual(common, sub)
 
 if __name__ == '__main__':
     unittest.main()
